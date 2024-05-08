a17f547669418b875b2d2df2fa8e6ce13889b999
IVDR123 <valentin123.ivdr@gmail.com>
2024-05-08 14:58:40 -0500

    m

--- src/main/java/com/proyecto/marketin/controller/AdministradorRestController.java
+++ src/main/java/com/proyecto/marketin/controller/AdministradorRestController.java
@@ -1,11 +1,11 @@
 package com.proyecto.marketin.controller;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +41,6 @@ public class AdministradorRestController {
 	public void register(@RequestBody RegisterRequest request){
 		
 		authService.register(request);
-		
 	}
 
 	@GetMapping(value = "usuarios")
@@ -67,20 +66,9 @@ public class AdministradorRestController {
 		authService.agregarPerfilAEmpleado(request);
 	}
 	
-	@GetMapping("/editarUsuario={username}?")
-	public List<Empleado> getEmpleado(@PathVariable String username) {
-		List<Empleado> empleado = new ArrayList();
-		Optional<Empleado> empleadoOptional = empleadoRepository.findByUsername(username);
-		if (empleadoOptional.isPresent()) {
-		    Empleado empleadoEncontrado = empleadoOptional.get();
-		    empleado.add(empleadoEncontrado);
-		} else {
-		    throw new RuntimeException("Error: El perfil no fue encontrado");
-		}
-		return empleado;
-	}
 	
-	@PostMapping("/editarUsuario?=true")
+	
+	@PostMapping("/editarUsuario")
 	public void editarEmpleado(@RequestBody EditarEmpleadoRequest request) {
 		authService.editarEmpleado(request);
 	}
